7406dd9ca0627c829fd31d4ff393c3197ab37fce
Emmanuel Obonyo <emmyobonyo@gmail.com>
2022-09-28 15:15:45 +0300

    Add products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
  import Header from "./components/Header";
  import { Container } from 'react-bootstrap'
  import Footer from "./components/Footer";
+ import HomeScreen from "./screens/HomeScreen";
  
  const App = () => {
   return (
@@ -9,7 +10,7 @@
       <Header />
       <main className="py-3">
         <Container>
-          <h2>Welcome to Proshop</h2>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
