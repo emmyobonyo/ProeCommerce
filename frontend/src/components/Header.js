2a034daa11c52b6eb7b563efa5146f2d4be29fab
Emmanuel Obonyo <emmyobonyo@gmail.com>
2022-09-29 14:47:04 +0300

    change routing

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Nav, Navbar, Container } from 'react-bootstrap'
 
 const Header = () => {
@@ -6,13 +7,20 @@ const Header = () => {
     <header>
         <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/">Pro Shop</Navbar.Brand>
+                <LinkContainer to='/'>
+                  <Navbar.Brand>ProShop</Navbar.Brand>
+                </LinkContainer>
+                <Navbar.Brand>Pro Shop</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                    <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
-                    <Nav.Link href="/login"><i className='fas fa-user'></i>Sign in</Nav.Link>
-                </Nav>
+                  <Nav className="ml-auto">
+                    <LinkContainer to='/cart'>
+                      <Nav.Link><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to='/login'>
+                      <Nav.Link><i className='fas fa-user'></i>Sign in</Nav.Link>
+                    </LinkContainer>
+                  </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
