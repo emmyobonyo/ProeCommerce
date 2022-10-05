36f0b989818324ca773e3497e8abe7c24431919b
Emmanuel Obonyo <emmyobonyo@gmail.com>
2022-10-05 13:25:35 +0300

    add some things

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,11 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import connectDB from './config/db.js';
+import dotenv from 'dotenv'
+import products from './data/products.js'
+
+dotenv.config();    
+
+connectDB();
 
 const app= express()
 
@@ -15,4 +21,6 @@ app.get('/api/products/:id', (req, res) => {
     const product = products.find(product => product._id === req.params.id)
     res.json(product)
 })
-app.listen(5000, console.log('Server running ...'))
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, console.log(`Server running on port ${PORT} and mode ${process.env.NODE_ENV}`))
