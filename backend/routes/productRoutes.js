d3e4824a7f97f8a180077d96c0d7e4998c07120c
Emmanuel Obonyo <emmyobonyo@gmail.com>
2022-10-07 20:08:17 +0300

    Add routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,20 +1,28 @@
 import express from "express";
-import Product from "../models/productModel";
+import Product from "../models/productModel.js";
 import asyncHandler from 'express-async-handler';
 
 const router = express.Router()
+
+// @desc     fetch all products 
+// @route    GET /api/products
+// @access   Public
 
 router.get('/', asyncHandler(async(req, res) => {
     const products = await Product.find({})
     res.json(products)
 }))
 
+// @desc     fetch a single product
+// @route    GET /api/products/:id
+// @access   Public
+
 router.get('/:id', asyncHandler(async(req, res) => {
-    const product = Product.findById(req.params.id)
+    const product = await Product.findById(req.params.id)
     if(product) {
         res.json(product)
     } else {
-        res.status(400).json({ msg: 'Product not found' })
+        res.status(404).json({message: "Product Not found"})
     }
 }))
 
--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,20 @@ connectDB();
 
 const app= express()
 
-app.use('/api/products', productRoutes)
-
 app.get('/', (req, res) => {
     res.send('API is running .......')
+})
+
+app.use('/api/products', productRoutes)
+
+app.use((err, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    res.status(statusCode)
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production'? null : err.stack, 
+    })
+
 })
 
 const PORT = process.env.PORT || 5000
